d7d45d3b67feb1165bae655d4f9f8f5ec71a2613
Lee Ibrahim <leeibrah@gmail.com>
2022-12-07 17:48:46 +0300

    fourth step: sort postgres port -6

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -2,57 +2,6 @@ import pytest
 from typing import List
 from app import schemas
 
-# def test_get_all_posts(authorized_client, test_posts):
-#     res = authorized_client.get("/posts/")
-
-#     def validate(post):
-#         return schemas.posts.PostwithVote(**post)
-
-#     posts_map = map(validate, res.json())
-#     post_list = list(posts_map)
-#     print(list(posts_map))
-#     print(test_posts[0].id)
-#     print(post_list[0].Post.id)
-
-#     # assert len(res.json()) == len(test_posts)
-#     assert res.status_code == 200
-#     assert post_list[0].Post.id == test_posts[0].id
-
-
-
-def test_unauthorized_user_get_all_posts(client, test_posts):
-    res = client.get("/posts/loggedin/user")
-
-    print(res.status_code)
-    assert res.status_code == 401
-
-
-
-def test_unauthorized_user_get_one_post(client, test_posts):
-    res = client.get(f"/posts/{test_posts[0].id}")
-
-    print(res.status_code)
-    assert res.status_code == 401
-
-
-def test_get_one_post_not_exist(authorized_client, test_posts):
-    res = authorized_client.get(f'/posts/88')
-
-    assert res.status_code == 404
-
-
-def test_get_one_post(authorized_client, test_posts):
-    res = authorized_client.get(f"/posts/{test_posts[0].id}")
-
-    print(res.json())
-
-    post = schemas.posts.PostwithVote(**res.json())
-
-    assert post.Post.id == test_posts[0].id
-    assert post.Post.title == test_posts[0].title
-    assert post.Post.content == test_posts[0].content
-    assert post.Post.published == test_posts[0].published
-
 
 @pytest.mark.parametrize("title, content, published", [
     ("Title Un", "C'est le titre un", True),
@@ -92,6 +41,62 @@ def test_create_post_default_published_true(authorized_client, test_user):
     assert created_post.content == "content of title 1"
     assert created_post.published == True
     assert created_post.user.id == test_user['id']
+
+
+def test_get_all_posts(authorized_client, test_posts):
+# def t_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.posts.PostwithVote(**post)
+
+    posts_map = map(validate, res.json())
+    post_list = list(posts_map)
+    print(list(posts_map))
+    print(test_posts[0].id)
+    print(post_list[0].Post.id)
+
+    # assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+    assert post_list[0].Post.id == test_posts[0].id
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+# def t_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/loggedin/user")
+
+    print(res.status_code)
+    assert res.status_code == 401
+
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+
+    print(res.status_code)
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f'/posts/88')
+
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+
+    print(res.json())
+
+    post = schemas.posts.PostwithVote(**res.json())
+
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.title == test_posts[0].title
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.published == test_posts[0].published
+
+
+
 
 
 def test_unauthorized_user_create_post(client, test_posts):
