1a7aedbc9f80ef381648886097ddd03dc28b2b3a
Lee Ibrahim <leeibrah@gmail.com>
2022-12-07 18:01:28 +0300

    fourth step: sort postgres port -9

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -68,20 +68,20 @@ def tes_unauthorized_user_get_all_posts(client, test_posts):
 
 
 
-def test_unauthorized_user_get_one_post(client, test_posts):
+def tes_unauthorized_user_get_one_post(client, test_posts):
     res = client.get(f"/posts/{test_posts[0].id}")
 
     print(res.status_code)
     assert res.status_code == 401
 
 
-def test_get_one_post_not_exist(authorized_client, test_posts):
+def tes_get_one_post_not_exist(authorized_client, test_posts):
     res = authorized_client.get(f'/posts/88')
 
     assert res.status_code == 404
 
 
-def test_get_one_post(authorized_client, test_posts):
+def tes_get_one_post(authorized_client, test_posts):
     res = authorized_client.get(f"/posts/{test_posts[0].id}")
 
     print(res.json())
